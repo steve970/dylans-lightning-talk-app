bbed60e4513f8811b0daa3355118636c75b49592
Dylan Ham <dylanham@gmail.com>
2015-02-24 16:17:15 -0700

    updated paths to go to github/auth
    Signed-off-by: Steve Hirschhorn <hirschhorn970@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :ensure_current_user
 
   def ensure_current_user
-    redirect_to signin_path unless current_user
+    redirect_to '/auth/github' unless current_user
   end
-  
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
