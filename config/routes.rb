e89e006025d749b83bb3bd312981ff143bb76be7
Dylan Ham <dylanham@gmail.com>
2015-02-23 20:30:24 -0700

    fix migration issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,62 +1,11 @@
 Rails.application.routes.draw do
 
-   root 'lightning_talks#index'
-   resources :lightning_talks
-
-<<<<<<< HEAD
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  root 'lightning_talks#index'
+  resources :lightning_talks
   resources :registrations, only: [:new, :create]
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-=======
-   get '/sign-in' => 'authentication#new', as: :signin
+  get '/sign-in' => 'authentication#new', as: :signin
   post '/sign-in' => 'authentication#create'
   get '/sign-out' => 'authentication#destroy', as: :signout
-  
->>>>>>> bee56e463b723c840aa3c12da3dacc3cc8a7d473
+
 end
