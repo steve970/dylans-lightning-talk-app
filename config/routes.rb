62397958e1255facbce0e6211eb224fb9bf8ba09
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-26 18:15:04 -0700

    added user-controller, show, routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   post '/sign-in' => 'authentication#create'
   get '/sign-out' => 'authentication#destroy', as: :signout
 
+  resources :users, only:[:show]
+
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
     resources :lightning_talks
+    resources :users, except:[:create, :new]
   end
 end
